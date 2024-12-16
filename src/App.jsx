59732e4ab03f2be3bf0464bfff48ced8d237ce07
Himaryy <122810667+Himaryy@users.noreply.github.com>
2024-12-16 11:59:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import HomePage from "./routes/homePage/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import { Layout } from "./routes/Layout/Layout";
+import SinglePage from "./routes/SinglePage/SinglePage";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
